71b712afa77a33b251eb31d899a5c3f2f898d834
Zibo Camito <zibo.camito@gmail.com>
2021-07-29 11:28:58 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,20 @@
-import { useState } from 'react';
-import './App.css';
-import { Display, DisplayProps } from './display/display';
-import { Keys } from './keys/keys';
-import { Digit, Operator, Operators } from './operator/operator';
-
-type AppState = DisplayProps & { reset: boolean };
-type StateMapper = (state: AppState) => AppState;
-type StateMapperFactory<T> = (param: T) => StateMapper;
-
-const initialState: AppState = {
-  partial: '0',
-  operator: Operators.Init,
-  value: '',
-  reset: false
-};
-
-const resetIfNeeded: StateMapper = state => state.reset ? initialState : state;
-
-const addDigit: StateMapperFactory<Digit> = digit => state => ({
-  ...resetIfNeeded(state),
-  value: state.reset ? digit : (state.value === '0' ? '' : state.value) + digit
-} as AppState);
-
-const addDecimalSeparator: StateMapper = state => ({
-  ...resetIfNeeded(state),
-  value: state.reset ? '0.' : (state.value === '' ? '0' : state.value) + '.',
-} as AppState);
-
-const calc = (state: AppState) => String(state.operator(Number(state.partial), Number(state.value)));
-
-const addOperator: StateMapperFactory<Operator> = operator => state => ({
-  partial: (!state.reset && state.value !== '') ? calc(state) : state.partial,
-  value: '',
-  operator,
-  reset: false
-} as AppState)
-
-const result: StateMapper = state => ({
-  ...state,
-  partial: calc(state),
-  reset: true
-} as AppState);
+import { useReducer } from "react";
+import "./App.css";
+import { initialState, reducer } from "./AppState";
+import { Display } from "./display/display";
+import { Keys } from "./keys/keys";
+import { Digit, Operator } from "./operator/operator";
 
 function App(): JSX.Element {
-  const [state, setState] = useState<AppState>(initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onDigit = (digit: Digit) =>
+    dispatch({ type: "addDigit", payload: digit });
+  const onDecimalSeparator = () => dispatch({ type: "addDecimalSeparator" });
+  const onOperator = (operator: Operator) =>
+    dispatch({ type: "addOperator", payload: operator });
+  const onEquals = () => dispatch({ type: "result" });
+  const onCancel = () => dispatch({ type: "reset" });
 
   return (
     <div id="app">
@@ -53,11 +24,11 @@ function App(): JSX.Element {
         value={state.value}
       ></Display>
       <Keys
-        onDigit={(digit: Digit) => setState(addDigit(digit))}
-        onDecimalSeparator={() => setState(addDecimalSeparator)}
-        onOperator={(operator: Operator) => setState(addOperator(operator))}
-        onEquals={() => setState(result)}
-        onCancel={() => setState(initialState)}
+        onDigit={onDigit}
+        onDecimalSeparator={onDecimalSeparator}
+        onOperator={onOperator}
+        onEquals={onEquals}
+        onCancel={onCancel}
       ></Keys>
     </div>
   );
--- src/display/display.tsx
+++ src/display/display.tsx
@@ -1,15 +1,5 @@
-import { Operator, Operators } from '../operator/operator';
-import './display.css'
-
-type OperatorString = '' | '+' | '-' | '*' | '/';
-
-const OperatorStrings = {
-  Init: '' as OperatorString,
-  Add: '+' as OperatorString,
-  Subtract: '-' as OperatorString,
-  Multiply: '*' as OperatorString,
-  Divide: '/' as OperatorString
-};
+import { Operator } from "../operator/operator";
+import "./display.css";
 
 export type DisplayProps = {
   partial: string;
@@ -21,19 +11,8 @@ export function Display(props: DisplayProps): JSX.Element {
   return (
     <div id="display">
       <div id="partial">{props.partial}</div>
-      <div id="operator">{displayOperator(props.operator)}</div>
+      <div id="operator">{props.operator.symbol}</div>
       <div id="value">{props.value}</div>
     </div>
   );
-}
-
-function displayOperator(operator: Operator): OperatorString | never {
-  switch (operator) {
-    case Operators.Init: return OperatorStrings.Init;
-    case Operators.Add: return OperatorStrings.Add;
-    case Operators.Subtract: return OperatorStrings.Subtract;
-    case Operators.Multiply: return OperatorStrings.Multiply;
-    case Operators.Divide: return OperatorStrings.Divide;
-    default: throw new Error('Invalid operator.');
-  }
 }
